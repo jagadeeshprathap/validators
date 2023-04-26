9e3ee46d3e7c0ae5ed4fff23cd7ee463ed86fb39
jagadeeshprathap <jagadeeshprathap9640@gmail.com>
2023-04-26 15:55:43 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,27 @@ def check_for_a(value):
 class studentform(forms.Form):
     name=forms.CharField(max_length=40,validators=[check_for_a])
     age=forms.IntegerField()
+    email=forms.EmailField()
+    re_enter_email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=50,widget=forms.HiddenInput,required=False)
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+
+        if e!=r:
+            raise forms.ValidationError('not matched')
+
+    def clean_age(self):
+        age=self.cleaned_data['age']
+
+        if age>9:
+            raise forms.ValidationError('not valid')
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+    
+        if len(bot)>0:
+            raise forms.ValidationError('no such data')
+
+        
--- app/views.py
+++ app/views.py
@@ -12,4 +12,6 @@ def student(request):
         SFD=studentform(request.POST)
         if SFD.is_valid():
             return HttpResponse(str(SFD.cleaned_data))
+        else:
+            return HttpResponse('data is not valid')
     return render(request,'student.html',d)
